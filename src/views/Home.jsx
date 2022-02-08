806d604a56488dd6b29cf205392b2f50f3e0a95a
Kevin Do <kevind0501@gmail.com>
2022-02-07 16:38:26 -0800

    added Providers as wrappers

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Form from '../components/Form';
 import List from '../components/List';
+import { UserProvider } from '../context/UserContext';
 
 export default function Home() {
   return (
-    <div>
+    <UserProvider>
       <Form />
       <List />
-    </div>
+    </UserProvider>
   );
 }
