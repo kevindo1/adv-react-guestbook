683c4639d33675f93161b5942b6c501cc7337ab1
Kevin Do <kevind0501@gmail.com>
2022-02-07 17:35:11 -0800

    worked on Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { useEntries } from '../context/EntriesContext';
+import { useUser } from '../context/UserContext';
+import { useState } from 'react';
 
 export default function Form() {
-  return <div>Form</div>;
+  const { user, setUser } = useUser();
+  const { entries, setEntries } = useEntries();
+  const [name, setName] = useState('');
+  const [newEntry, setNewEntry] = useState('');
+
+  const updateGuest = () => {
+    if (!newEntry) return;
+    setUser(name);
+    setEntries([...entries, { name, newEntry }]);
+    setNewEntry('');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateGuest();
+  };
+
+  return (
+    <>
+      <form>
+        <label>
+          <h4>Name</h4>
+          <input type="text" placeholder="Name"></input>
+        </label>
+        <label>
+          <h4>Message</h4>
+          <input type="text" placeholder="Message"></input>
+        </label>
+        <button>Sign</button>
+      </form>
+    </>
+  );
 }
