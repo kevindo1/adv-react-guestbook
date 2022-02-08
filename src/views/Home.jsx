27c761be52990c4e0cae05496cc12d4634fc8fd9
Kevin Do <kevind0501@gmail.com>
2022-02-07 16:24:12 -0800

    added userContext

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../components/Form';
+import List from '../components/List';
+
+export default function Home() {
+  return (
+    <div>
+      <Form />
+      <List />
+    </div>
+  );
+}
