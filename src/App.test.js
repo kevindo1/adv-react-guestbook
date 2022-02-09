d4d1d51c8b33b0bc5b6d8e1475077d2cb0a27a11
Kevin Do <kevind0501@gmail.com>
2022-02-09 12:28:03 -0800

    passing behavioural test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import { UserProvider } from './context/UserContext';
 
-test.skip('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Should be able to sign the book', () => {
+  render(
+    <UserProvider>
+      <App />
+    </UserProvider>
+  );
+
+  //grab all the consts for the input box
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const name = 'Kevin';
+  const messageInput = screen.getByRole('textbox', { name: /message/i });
+  const message = 'hello';
+  const button = screen.getByRole('button', { name: /sign/i });
+
+  // type the name and message, click the sign button
+  userEvent.type(nameInput, name);
+  userEvent.type(messageInput, message);
+  userEvent.click(button);
+
+  // the results that user gets back
+  expect(screen.getByRole('heading', { name: /name: kevin/i })).toBeInTheDocument();
+  expect(screen.getByText(/message: hello/i)).toBeInTheDocument();
 });
