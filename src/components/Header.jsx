20c34916ce7423e828d58733765378cc288aeb14
Kevin Do <kevind0501@gmail.com>
2022-02-09 14:54:21 -0800

    added dark mode hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { useUser } from '../context/UserContext';
+import DarkModeToggle from '../DarkMode/DarkModeToggle';
+import '../DarkMode/DarkMode.css';
 
 export default function Header() {
   const { users } = useUser();
 
-  return <div>Welcome {users ? users : 'Guest'}!</div>;
+  return (
+    <div>
+      <DarkModeToggle />
+      Welcome {users ? users : 'Guest'}!
+    </div>
+  );
 }
