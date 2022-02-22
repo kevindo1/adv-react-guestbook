ead6070286b0b2e6c4429f6098e0a17c5de4c195
Kevin Do <kevind0501@gmail.com>
2022-02-22 10:27:35 -0800

    wrapped privateroute around home page and created auth file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
 import Home from './views/Home';
+import Auth from './views/Auth';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login">
+          <Auth />
+        </Route>
+        <PrivateRoute>
+          <div className="App">
+            <Home />
+          </div>
+        </PrivateRoute>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
