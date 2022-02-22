468200f223a1593c1e9839523ee388d00d0f39c1
Kevin Do <kevind0501@gmail.com>
2022-02-22 10:29:25 -0800

    made backbone of the login form

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function Auth() {
-  return <div>Auth</div>;
+  return (
+    <div>
+      <h3>Login</h3>
+      <form>
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
