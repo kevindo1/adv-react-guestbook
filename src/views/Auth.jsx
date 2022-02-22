6cc1e834d243742fad255aba802ed26471c62950
Kevin Do <kevind0501@gmail.com>
2022-02-22 10:45:11 -0800

    wrote some logic inside auth

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useUser } from '../context/UserContext';
 
 export default function Auth() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { setUsers } = useUser();
+  const history = useHistory();
+  const [error, setError] = useState(null);
+  const { from } = location.state || { from: { pathname: '/' } };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (
+      username === process.env.REACT_APP_AUTH_USERNAME &&
+      password === process.env.REACT_APP_AUTH_PASSWORD
+    ) {
+      setUsers({ username: username, password: password });
+      setUsername('');
+      setPassword('');
+      history.replace(from);
+    } else {
+      setError('Login failed. Try again.');
+    }
+  };
+
   return (
     <div>
       <h3>Login</h3>
-      <form>
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
+      <form onSubmit={handleLogin}>
+        <label>
+          <input
+            type="text"
+            placeholder="Email"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          <input
+            type="text"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
         <button>Login</button>
       </form>
+      {error}
     </div>
   );
 }
